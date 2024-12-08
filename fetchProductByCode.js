78f6359b6d69d3d4440eb017d5f2bb2ecb232533
PedroAugustoMartinsG <71160689+PedroAugustoMartinsG@users.noreply.github.com>
2024-12-08 10:30:18 -0300

    Update fetchProductByCode.js
    Adicionar gestão de campanhas

--- fetchProductByCode.js
+++ fetchProductByCode.js
@@ -21,7 +21,8 @@ function initializeProductMap(sheetByName) {
             itemName: data[i][1],
             itemBrand: data[i][2],
             selectedCategory: data[i][3],
-            itemWeight: data[i][4]
+            itemWeight: data[i][4],
+            selectedProductUnit: data[i][5]
         });
     }
 }
@@ -31,10 +32,9 @@ function initializeProductMap(sheetByName) {
  * e busca o último produto inserido a partir do código de barras
  * @return {Object|null} Retorna os dados do produto ou null se não for encontrado.
  */
-function searchLastInsertedItem(searchBarCode = 123456789) {
+function searchLastInsertedItem(searchBarCode = 10000000) {
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Planilha de Registro");
-    const data = sheet.getDataRange().getValues();       
-    let result = {}
+    const data = sheet.getDataRange().getValues();
     // Itera a partir da segunda linha, assumindo que a primeira linha é o cabeçalho
     for (let i = data.length - 1; i > 1; i--) {
         const searchBarCodeString = String(searchBarCode);
@@ -45,15 +45,12 @@ function searchLastInsertedItem(searchBarCode = 123456789) {
             itemCode: data[i][0],
             itemQuantity: data[i][1],
             itemInsertHour: String(data[i][2]),
-            campaignId: data[i][3]
+            campaignId: String(data[i][3])
           }
-          result = resultAux
-          break
+          return resultAux
         }
     }
-    console.log(result)  
-    return result || null
-    
+    return null
 }
 
 /**
@@ -71,3 +68,37 @@ function fetchProductByCode(barcode = 7891000055246) {
     barcode = String(barcode);
     return productMap.get(barcode) || null;
 }
+
+function fetchProductByCodeRegistro(barcode = 10000000) {
+    // Certifique-se de que o `Map` está carregado antes de fazer a busca
+    if (productMap.size === 0) {
+        initializeProductMap("Planilha de Registro");
+    }
+
+    // Converte o código de barras para string e busca no `Map`
+    barcode = String(barcode);
+    return productMap.get(barcode) || null;
+}
+
+function getLastCampaignValue() {
+  try {
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Gestão de Campanhas");
+    
+    // Obtenha o intervalo com todos os dados da última linha preenchida
+    const lastRow = sheet.getLastRow();
+    if (lastRow === 0) {
+      return "A planilha está vazia.";
+    }
+
+    const lastRowValues = sheet.getRange(lastRow, 1, 1, sheet.getLastColumn()).getValues()[0];
+   
+    // Retorna o último valor não vazio da linha
+    const lastValue = lastRowValues[0];
+  
+
+    return lastValue || "Indefinido";
+  } catch (error) {
+    return `Erro ao obter a última campanha: ${error.message}`;
+  }
+}
+
